65782f2884f7774bbe31e59325f29704707e5734
Zinadin Zidan <tuhinsultana075@gmail.com>
2020-04-20 00:51:24 +0600

    ulysse-common: Use Apn Config From Los
    Change-Id: If54abc6c6f8a74ee86617bd5bfafeead6e7bf9be

--- msm8937.mk
+++ msm8937.mk
@@ -73,6 +73,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
+# APNS Config
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/apns-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
+
 # ANT
 PRODUCT_PACKAGES += \
     AntHalService \
