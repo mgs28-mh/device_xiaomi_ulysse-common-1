3b67ed2d030adf3848ea23c6d7ec0b2b2f54f110
zqm64 <zqm64@outlook.com>
2020-10-22 12:26:07 +0800

    MOD: msm8937-common: init: Add redfin RD1A.200810.020 build FP

--- init/init_msm8937.cpp
+++ init/init_msm8937.cpp
@@ -111,4 +111,9 @@ void vendor_load_properties()
     property_set("dalvik.vm.heaptargetutilization", heaptargetutilization);
     property_set("dalvik.vm.heapminfree", heapminfree);
     property_set("dalvik.vm.heapmaxfree", heapmaxfree);
+
+    // Pixel identification
+    property_override("ro.build.description","redfin-user 11 RD1A.200810.020 6792687 release-keys");
+    property_override("ro.build.fingerprint","google/redfin/redfin:11/RD1A.200810.020/6792687:user/release-keys");
+    property_override("ro.system.build.fingerprint","google/redfin/redfin:11/RD1A.200810.020/6792687:user/release-keys");
 }
