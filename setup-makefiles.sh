d25a0092beb74fac16a7ca18446c088158dca8f4
mgs28-mh <gsaputra880@gmail.com>
2022-04-12 00:00:05 +0700

    ulysse-common: move most blobs to proprietary-files
    - Also update most blobs from
      * Daisy-user 10 QKQ1.191002.002 V11.0.21.0.QDLMIXM release-keys
      * Xiaomi/olive/olive:10/QKQ1.191014.001/V12.5.1.0.QCNCNXM:user/release-keys
    
    Change-Id: Ie06cdbf2f814f09bc6d4f8cc396afb72062a458b

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -18,7 +18,10 @@
 
 set -e
 
-INITIAL_COPYRIGHT_YEAR=2020
+INITIAL_COPYRIGHT_YEAR=2022
+
+DEVICE_COMMON=ulysse-common
+VENDOR=xiaomi
 
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
@@ -26,7 +29,7 @@ if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
 LINEAGE_ROOT="$MY_DIR"/../../..
 
-HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+HELPER="../../../extract_utils.sh"
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -40,6 +43,7 @@ setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" true
 write_headers "ugglite ugg ulysse"
 
 # The standard common blobs
+write_makefiles "$MY_DIR"/proprietary-files.txt true
 write_makefiles "$MY_DIR"/proprietary-files-qc.txt true
 
 # We are done!
