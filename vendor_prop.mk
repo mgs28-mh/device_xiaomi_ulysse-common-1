adislice/android_device_xiaomi_ulysse-common

ulysse-common: Replace BOARD_CHARGER_DISABLE_INIT_BLANK with equivalent prop
Change-Id: I00125a429cde213e8dd22f742c5875e39b80bb5e

vendor_prop.mk
@@ -68,6 +68,7 @@ vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.motorola.faceunlock,com
 
 # Charger
 PRODUCT_PRODUCT_PROPERTIES += \
+ro.charger.disable_init_blank=true \
 ro.charger.enable_suspend=true
 
 # Cne/Dpm

mgs28-mh/device_xiaomi_ulysse-common-1

vendor_prop.mk
@@ -68,7 +68,8 @@ vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.motorola.faceunlock,com
 
 # Charger
 PRODUCT_PRODUCT_PROPERTIES += \
-ro.charger.enable_suspend=true
+ro.charger.enable_suspend=true \
+ro.charger.disable_init_blank=true
 
 # Cne/Dpm
 PRODUCT_PROPERTY_OVERRIDES += \